2b6e6d5d8af7b4143088fbde90843341c8ead82b
Cordell <cordellbonnieux@gmail.com>
2022-01-16 15:42:23 -0800

    Added Login.jsx and added two fonts (via google).

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login.jsx';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div id="content">
+        <Login />
+      </div>
     </div>
   );
 }
---
+++ src/Login.jsx
@@ -0,0 +1,11 @@
+
+export default function Login() {
+    return (
+        <div id="login_container">
+            <p>
+                Hey this works!
+            </p>
+            <h1>another font</h1>
+        </div>
+    );
+}
