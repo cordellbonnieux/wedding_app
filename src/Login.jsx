f8fee26303b885058e2a202fb2f35375ae70c865
Cordell <cordellbonnieux@gmail.com>
2022-01-16 16:17:15 -0800

    Built login page, and created css classes for the containers.

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,19 @@
 
 export default function Login() {
     return (
-        <div id="login_container">
-            <p>
-                Hey this works!
-            </p>
-            <h1>another font</h1>
+        <div id="login" className="main">
+            <div id="login_container" className="container">
+                <div id="login_container_inner" className="container_inner">
+                    <h1 id="login_title">You've been invited to a wedding!</h1>
+                    <p id="login_text">
+                        Please enter the passcode you've been provided to continue.
+                    </p>
+                    <form id="login_form">
+                        <input type="text" id="login_pass_text" />
+                        <input type="submit" value="enter" id="login_pass_submit" className="button" />
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
